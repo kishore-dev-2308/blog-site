06c89b9435d68a771f8014bd398eb4d511a9308d
kishore <kishore82208@gmail.com>
2026-02-09 20:18:20 +0530

    debug

--- client/src/api/apiBase.js
+++ client/src/api/apiBase.js
@@ -17,10 +17,11 @@ export const handleGlobalErrors = (error) => {
   }
 
   if (status >= 500) {
-    const path = window.location.pathname;
-    if (path.startsWith("/admin")) window.location.href = "/admin/500";
-    else if (path.startsWith("/author")) window.location.href = "/author/500";
-    else window.location.href = "/500";
+    // const path = window.location.pathname;
+    // if (path.startsWith("/admin")) window.location.href = "/admin/500";
+    // else if (path.startsWith("/author")) window.location.href = "/author/500";
+    // else window.location.href = "/500";
+    console.log("Server error occurred:", error);
   }
 
   return Promise.reject(error);
